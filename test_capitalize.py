db523a7f3d2dbd0d83a2463a744f4f73d0f037d3
Sharpless512 <mahoney_be@hotmail.com>
2017-07-26 10:43:03 +0200

    testing some stuff

--- test_capitalize.py
+++ test_capitalize.py
@@ -1,7 +1,12 @@
-import pytest
+import pytest, os
+
+from igdb_api_python.igdb import igdb as igdb
 
 def inc(x):
     return x + 1
 
 def test_answer():
     assert inc(3) == 4
+
+#print(os.environ['api_key'])
+i = igdb(os.environ['api_key'])
