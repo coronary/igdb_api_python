788bd31f3e8d66de73f36a1d764bf438e3831599
Sharpless512 <mahoney_be@hotmail.com>
2017-07-26 10:57:20 +0200

    add first tests

---
+++ test_games.py
@@ -0,0 +1,12 @@
+import pytest, os
+
+from igdb_api_python.igdb import igdb as igdb
+
+igdb = igdb(os.environ['api_key'])
+
+def test_singleGame():
+    result = igdb.callApi("games", ids=1942)
+    assert result.status_code == 200
+def test_multipleGames():
+    result = igdb.callApi("games",ids=[27193,23212,1942])
+    assert result.status_code == 200
