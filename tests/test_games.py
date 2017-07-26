955c7738638351affc98f022dbabe36294765e24
Sharpless512 <mahoney_be@hotmail.com>
2017-07-26 14:28:48 +0200

    try some test

--- tests/test_games.py
+++ tests/test_games.py
@@ -5,8 +5,7 @@ from igdb_api_python.igdb import igdb as igdb
 igdb = igdb(os.environ['api_key'])
 
 def test_singleGame():
-    result = igdb.callApi("games", 1942)
-    assert result.status_code == 200
+    result = igdb.games(1942)
 def test_multipleGames():
     result = igdb.callApi("games",{
         'ids':[27193,23212,1942]
